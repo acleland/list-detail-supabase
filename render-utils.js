0b5b5fd1e3e071f0e4693f63477909a7717a06a1
Andrea Cleland <andreaclelan@gmail.com>
2022-02-13 16:56:51 -0800

    everything working, but i'm unhappy with styles

--- render-utils.js
+++ render-utils.js
@@ -7,9 +7,43 @@ export function renderDonutShop(shop) {
     const h3 = document.createElement('h3');
     h3.textContent = shop.name;
 
+    const image = document.createElement('div');
+    image.classList.add('image-container');
+    const img = document.createElement('img');
+    img.src = `..${shop.image}`;
+    image.append(img);
+    
+    a.append(h3, image);
+    return a;
+}
+
+export function renderShopDetail(shop) {
+    const div = document.createElement('div');
+    div.classList.add('shop-detail');
+    
+    const h1 = document.createElement('h1');
+    h1.classList.add('detail-title'); 
+    h1.textContent = shop.name;
+    
+    const image = document.createElement('div');
+    image.classList.add('detail-image');
     const img = document.createElement('img');
     img.src = shop.image;
+    image.append(img);
+
+    const ul = document.createElement('ul');
+    ul.classList.add('detail-info');
     
-    a.append(h3, img);
-    return a;
+    const phone = document.createElement('li');
+    phone.textContent = shop.phone;
+    const address = document.createElement('li');
+    address.textContent = shop.address;
+    const website = document.createElement('li');
+    website.textContent = shop.website;
+    
+
+    ul.append(phone, address, website);
+
+    div.append(h1, image, ul);
+    return div;
 }
--- test/my.test.js
+++ test/my.test.js
@@ -1,17 +1,18 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-import { renderDonutShop } from '../render-utils.js';
+import { renderDonutShop, renderShopDetail } from '../render-utils.js';
 import { getDonutShops, getDonutShop } from '../fetch-utils.js';
 
 
 const test = QUnit.test;
-const skip = QUnit.skip;
+// const skip = QUnit.skip;
 
 test('test getDonutShops', async(expect) => {
-    const expected = 'Coco Donuts';
+    const expected = 'Voodoo Doughnut';
     const shops = await getDonutShops();
     const actual = shops[0].name;
     expect.equal(actual, expected);
+    expect.equal(shops.length, 5);
 });
 
 test('test getDonutShop', async(expect) => {
@@ -21,15 +22,21 @@ test('test getDonutShop', async(expect) => {
     expect.equal(actual, expected);
 });
 
+test('test renderShopDetail', async(expect) => {
+    const expected = `<div class="shop-detail"><h1 class="detail-title">Coco Donuts</h1><div class="detail-image"><img src="/assets/coco.png"></div><ul class="detail-info"><li>(971) 302-7445</li><li>4790 SE Milwaukie Ave, Portland, Oregon 97202</li><li>https://www.cocodonuts.com/</li></ul></div>`;
+    const shop = await getDonutShop(1);
+    const actual = renderShopDetail(shop);
+    expect.equal(actual.outerHTML, expected);
+});
 
-skip('test renderDonutShop', (expect) => {
+test('test renderDonutShop', async(expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<a class="donut-shop" href="/shop-detail/?id=1"><h3>Coco Donuts</h3><img src="/assets/coco.png"></a>`;
-    
+    const expected = `<a class="donut-shop" href="/shop-detail/?id=1"><h3>Coco Donuts</h3><div class="image-container"><img src="../assets/coco.png"></div></a>`;
+    const shop = await getDonutShop(1);
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderDonutShop();
+    const actual = renderDonutShop(shop);
 
     //Expect
     // Make assertions about what is expected versus the actual result
